77a3d0e4af0d6afe1d910cb84ab1d72d0f9191e8
Tomáš Gallischek <t.gall@seznam.cz>
2026-09-14 08:26:35 +0200

    api

--- app_dm_frontend/views.py
+++ app_dm_frontend/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+import requests
+from django.http import HttpResponse
 
 def index(request):
+    backend_url = 'http://127.0.0.1:8000/api/ziskej-data/' 
 
-    return render(request, 'app_dm_frontend/index.html')
+    try:
+        odpoved = requests.get(backend_url)
+        
+        if odpoved.status_code == 200:
+            data = odpoved.json() 
+            data_list = data.get("data", [])
+
+            return HttpResponse(f"Úspěšně přijato z backendu: {data_list}")
+        else:
+            return HttpResponse(f"Backend vrátil chybu: {odpoved.status_code}")
+
+    except requests.exceptions.ConnectionError:
+        return HttpResponse("Nepodařilo se připojit k backendu. Běží atarax_backend?")
