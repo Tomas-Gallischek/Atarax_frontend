bfa9f99bfa07036d685f0961869b63fbe7115ac3
Tomáš Gallischek <t.gall@seznam.cz>
2026-09-14 10:46:19 +0200

    aktualizace kódu (14.09.2026 10:46)

--- app_dm_frontend/urls.py
+++ app_dm_frontend/urls.py
@@ -6,6 +6,7 @@ app_name = 'app_dm_frontend'
 urlpatterns = [
     path('', views.index, name='index'),
     path('maps/', views.maps, name='maps'),
+    path('maps_detail/<str:name>/', views.maps_detail, name='maps_detail'),
     path('npc/', views.npc, name='npc'),
     path('diary/', views.diary, name='diary'),
     path('glem/', views.glem, name='glem'),
--- app_dm_frontend/views.py
+++ app_dm_frontend/views.py
@@ -53,6 +53,33 @@ def maps(request):
     except requests.exceptions.ConnectionError:
         return HttpResponse("Nepodařilo se připojit k backendu. Běží atarax_backend?")
 
+def maps_detail(request, name):
+    print(f'spuštění map_detail s parametrem {name}')
+
+# PTÁM SE TORCHU NEEFEKTIVNĚ NA VŠECHNY MAPY V DATABÁZI ALE SERVER JE DOST RYCHLÝ ABY TO ZVLÁDL
+
+    try:
+        api_response = requests.get(BACKEND_URL + "/maps")
+        print(f"MAPY: {API_CON_OK}")
+        
+        if api_response.status_code == 200:
+            print(f"MAPY: {API_DATA_OK}")
+
+            data = api_response.json()
+            
+            for item in data.get('kingdoms', []) + data.get('continents', []) + data.get('regions', []) + data.get('cities', []) + data.get('dungeons', []) + data.get('unique_locations', []):
+                if item.get('name') == name:
+                    return render(request, 'app_dm_frontend/maps_detail.html', {'item': item})
+
+            return HttpResponse(f"Mapa '{name}' nebyla nalezena ve všech kategoriích.")
+            
+        else:
+            return HttpResponse(f"Backend vrátil chybu: {api_response.status_code}")
+
+    except requests.exceptions.ConnectionError:
+        return HttpResponse("Nepodařilo se připojit k backendu. Běží atarax_backend?")
+
+
 def npc(request):
     print('spuštění metody npc')
     return render(request, 'app_dm_frontend/npc.html')
